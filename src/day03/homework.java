6ed4b800e5fc7aa663c56111ff97a75d07743942
2083904592 <2083904592@qq.com>
2020-06-17 10:40:01 +0800

    作业完成提交20200617

--- src/day03/homework.java
+++ src/day03/homework.java
@@ -1,14 +1,31 @@
 package day03;
-
+import java.util.Scanner;
 public class homework {
-
-
     public static void main(String[] args) {
         //99乘法表
-        System.out.println("打印成功");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + (i * j) + "\t");
+            }
+            System.out.println();
+        }
         //打印等腰三角形
-        System.out.println("打印成功");
-        System.out.println("利用for循环嵌套");
+        int rows;    //三角形行数
+        System.out.print("请输入等腰三角形的行数：");
+        Scanner input = new Scanner(System.in);
+        rows = input.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
-
 }
+
+
+
+
